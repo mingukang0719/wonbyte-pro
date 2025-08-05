60b17f80a71cf462a38cf7d06640f4f86ae54929
mingukang0719 <153787118+mingukang0719@users.noreply.github.com>
2025-08-05 20:06:00 +0900

    Add missing LoadingSpinner component

---
+++ src/components/common/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function LoadingSpinner({ size = 'default', className = '' }) {
+  const sizeClasses = {
+    small: 'h-4 w-4',
+    default: 'h-8 w-8',
+    large: 'h-12 w-12'
+  }
+
+  return (
+    <div className={`animate-spin rounded-full border-b-2 border-blue-600 ${sizeClasses[size]} ${className}`}></div>
+  )
+}
