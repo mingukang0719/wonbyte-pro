99ea31ca4c2eebed2ec6bf82fb670e671557268e
mingukang0719 <153787118+mingukang0719@users.noreply.github.com>
2025-08-08 16:19:36 +0900

    Fix routing issue by using HashRouter instead of BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './components/auth/PrivateRoute'
 
